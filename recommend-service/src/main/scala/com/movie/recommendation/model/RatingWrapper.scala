9861d2208136fca3a31d6870a4fcf0c64e1efb89
Zak Hassan <zak.hassan1010@gmail.com>
2016-11-10 17:55:50 -0500

    Removing spray ‘recommend-service’ replacing with Play Framework.

--- recommend-service/src/main/scala/com/movie/recommendation/model/RatingWrapper.scala
+++
@@ -1,9 +0,0 @@
-package com.movie.recommendation.model
-
-import org.apache.spark.mllib.recommendation.Rating
-
-/**
-  * Created by zhassan on 2016-11-08.
-  */
-case class RatingWrapper (val rating:List[Rating], val totalRecords:Int, val totalPages:Int)
-
--- recommend-web/public/app/view1/view1.js
+++ recommend-web/public/app/view1/view1.js
@@ -9,11 +9,26 @@ angular.module('myApp.view1', ['ngRoute'])
     });
 }])
 
-.controller('View1Ctrl', ['movieDataService', view1Ctrl]);
+.controller('View1Ctrl', ['$scope','movieDataService', view1Ctrl]);
 
-function view1Ctrl(movieDataService) {
+function view1Ctrl($scope, movieDataService) {
     var that = this;
+
+    $scope.items = []
+
+    for(var i =0 ;i<30;i++){
+      $scope.items.push({id:i,label:""+i});
+    }
+
+    $scope.userSelect = $scope.items[0];
+
     movieDataService.getMovieById(1).then(function(ratings) {
         that.topRatedMovies = ratings;
     });
+
+    $scope.loadList=function(item){
+       movieDataService.getMovieById(item.id).then(function(ratings) {
+          that.topRatedMovies = ratings;
+      });
+    };
 }
