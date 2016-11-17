61c4f4a902c496ef558b1c274c78135cdbdbb959
Zak Hassan <zak.hassan1010@gmail.com>
2016-11-17 15:07:01 -0500

    Adding ability to create custom dataset for rating mllib

--- recommend-web/app/repository/RatingRepo.scala
+++ recommend-web/app/repository/RatingRepo.scala
@@ -5,6 +5,7 @@ package repository
  import org.mongodb.scala._
  import org.mongodb.scala.bson.collection.immutable.Document
  import repository.Helpers._
+import java.io._
 /**
   * Created by zhassan on 2016-11-15.
   */
@@ -29,11 +30,32 @@ object RatingRepo {
     //TODO: Need to inject configurations instead of hardcoded localhost mongo
     val mc = MongoClient(DB_MONGO_URI)
     //TODO: should make these lazy val's
-    val collection= RatingRepo.connect(mc)
+    val collection= connect(mc)
     val doc: Document = RateToDocument(rating)
     collection.insertOne(doc).results()
 //    collection.find.first().printResults()
     mc.close()
+  }
+
+
+  /**
+    * Exports all ratings from internal table to an csv file which we will use to run our machine learning on.
+    */
+  def exportRatingsFromMongo(): Unit ={
+    val mc = MongoClient(DB_MONGO_URI)
+    //TODO: should make these lazy val's
+    val collection= connect(mc)
+    val ratings=collection.find().results().map((x) => x )
+
+    val pw = new PrintWriter(new File("/tmp/custom_ratings.txt"))
+    ratings.foreach(x => {
+      val user= x.get("user").get.asInt32().getValue
+      val product=x.get("product").get.asInt32().getValue
+      val rating=x.get("rating").get.asDouble().getValue
+      val ln=sys.props("line.separator")
+      pw.write(s"$user::$product::$rating$ln")
+    })
+    pw.close()
   }
 
 
