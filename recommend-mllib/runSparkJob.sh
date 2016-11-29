0ad8ea4579a0f1c1a2e990adea36bcc2e08d50b7
Zak Hassan <zak.hassan1010@gmail.com>
2016-11-29 13:04:12 -0500

    Adding Kubernetes Job to project

--- recommend-mllib/runSparkJob.sh
+++ recommend-mllib/runSparkJob.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname "$0")
-
-/usr/local/spark/bin/spark-submit  --master  spark://zhassan.yyz.redhat.com:7077 --class com.example.data.analytics.App $BASEDIR/target/recommend-mllib-1.0.0-SNAPSHOT-jar-with-dependencies.jar  --rank 5 --numIterations 5 --lambda 1.0 --infinispanHost 127.0.0.1 --kryo /usr/local/spark/data/mllib/sample_movielens_data.txt
+SPARK_HOME=/usr/loca/spark
+$SPARK_HOME/bin/spark-submit  --master  $SPARK_MASTER_URL --class com.example.data.analytics.App $BASEDIR/target/recommend-mllib-1.0.0-SNAPSHOT-jar-with-dependencies.jar  --rank 5 --numIterations 5 --lambda 1.0 --infinispanHost $RECOMMEND_SERVICE_SERVICE_HOST --kryo $SPARK_HOME/data/mllib/sample_movielens_data.txt
